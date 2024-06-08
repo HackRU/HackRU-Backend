9600f8577ad730f8ffd38a7725982df3d6055142
Ethan Nguyen <en256@scarletmail.rutgers.edu>
2024-06-08 00:39:16 -0400

    add cors

--- src/functions/authorize/index.ts
+++ src/functions/authorize/index.ts
@@ -8,6 +8,7 @@ export default {
       http: {
         method: 'post',
         path: 'authorize',
+        cors: true,
         request: {
           schemas: {
             'application/json': schema,
