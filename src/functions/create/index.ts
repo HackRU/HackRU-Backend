91620ac5478817df9afb4b98f5560d5397da73d0
DaStormer <dastormer@stormdevelopmentz.xyz>
2024-06-10 15:56:03 -0400

    empty strings for undefined user props

--- src/functions/create/index.ts
+++ src/functions/create/index.ts
@@ -1,6 +1,6 @@
+import { handlerPath } from '@libs/handler-resolver';
 import schema from './schema';
 
-import { handlerPath } from '@libs/handler-resolver';
 export default {
   handler: `${handlerPath(__dirname)}/handler.main`,
   events: [
