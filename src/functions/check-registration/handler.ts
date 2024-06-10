1fffe328215a61a44af45330ef9ecb0a872d42c9
DaStormer <dastormer@stormdevelopmentz.xyz>
2024-06-09 22:08:11 -0400

    resolve issues

--- src/functions/check-registration/handler.ts
+++ src/functions/check-registration/handler.ts
@@ -7,13 +7,8 @@ import schema from './schema';
 import { MongoDB } from '../../util';
 import * as config from '../../config'; // eslint-disable-line
 
-const checkRegistration: ValidatedEventAPIGatewayProxyEvent<
-  typeof schema
-> = async (event) => {
-  const registrationStatus = await queryByEmail(
-    event.body.email,
-    config.DEV_MONGO_URI
-  );
+const checkRegistration: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
+  const registrationStatus = await queryByEmail(event.body.email, config.DEV_MONGO_URI);
   return {
     statusCode: 200,
     body: JSON.stringify({
@@ -23,10 +18,7 @@ const checkRegistration: ValidatedEventAPIGatewayProxyEvent<
   };
 };
 
-async function queryByEmail(
-  email: string,
-  mongoURI: string
-): Promise<string | null> {
+async function queryByEmail(email: string, mongoURI: string): Promise<string | null> {
   // Connect to MongoDB
   try {
     const db = MongoDB.getInstance(mongoURI);
@@ -34,9 +26,7 @@ async function queryByEmail(
     const client = db.getClient();
 
     // Access the database and collection
-    const collection = client
-      .db('dev')
-      .collection(config.DB_COLLECTIONS['users']);
+    const collection = client.db('dev').collection(config.DB_COLLECTIONS['users']);
 
     // Query the object based on the email
     const result = await collection.findOne({ email });
