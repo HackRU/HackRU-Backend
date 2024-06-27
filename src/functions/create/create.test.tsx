13e4d2a2f771f78439f632945ef9737046cc94e2
poojakedia <poojakedianov@gmail.com>
2024-06-27 17:35:27 -0400

    Added and fixed all tests for /create endpoint

--- src/functions/create/create.test.tsx
+++ src/functions/create/create.test.tsx
@@ -1,4 +1,4 @@
-import { afterEach, describe, expect, it } from "@jest/globals"
+import { describe, expect, it } from "@jest/globals"
 
 
 import * as path from 'path';
@@ -6,54 +6,61 @@ import * as dotenv from 'dotenv';
 dotenv.config({ path: path.resolve(process.cwd(), '.env') });
 import { main } from './handler';
 
-const bcrypt = require('bcryptjs');
-jest.mock('bcryptjs');
 
-//const MongoDB = require('../../util');
-jest.mock('../../util', () => ({
-    MongoDB: {
-        getInstance: jest.fn().mockReturnValue({
-            connect: jest.fn(),
-            disconnect: jest.fn(),
-            getCollection: jest.fn().mockReturnValue({
-                findOne: jest.fn(),
-                insertOne: jest.fn(),
-        })  
-        
-})
-}}));
+interface Event {
+    body: string;
+    path: string;
+    httpMethod: string;
+    headers: Record<string, string>;
+    multiValueHeaders: Record<string, string[]>; 
+    isBase64Encoded: boolean;
+    pathParameters: null | Record<string, string>;
+    queryStringParameters: null | Record<string, string>;
+    multiValueQueryStringParameters: null | Record<string, string[]>;
+    requestContext: null; 
+    resource: string;
+    stageVariables: null | Record<string, string>;
+    rawBody: string; 
+  }
 
-afterEach(()=>{
-    jest.resetAllMocks();
-})
-
-
-describe("Create endpoint", () => {
-    it('Create a new user', async() =>{
-
-        const mockEvent = {
-            body: JSON.stringify({
-                email: 'testEmail@gmail.com',
-                password: 'testPassword123',
-            }),
+// creating mock event for all tests
+function createMockEvent(userData: Record<string, string>, path: string, httpMethod: string): Event {
+    return{
+            body: JSON.stringify(userData),
             headers: {},
             multiValueHeaders: {},
-            httpMethod: 'POST',
+            httpMethod: httpMethod,
             isBase64Encoded: false,
-            path: '',
+            path: path,
             pathParameters: null,
             queryStringParameters: null,
             multiValueQueryStringParameters: null,
             stageVariables: null,
             requestContext: {} as any,
             resource: '',
-            rawBody: JSON.stringify({
-                email: 'testEmail@gmail.com',
-                password: 'testPassword123',
-            }),
-        };
+            rawBody: JSON.stringify(userData),
+    }
+};
+interface Context {
+    callbackWaitsForEmptyEventLoop: boolean,
+    functionName: string,
+    functionVersion: string,
+    invokedFunctionArn: string,
+    awsRequestId: string,
+    logGroupName: string,
+    logStreamName: string,
+    memoryLimitInMB: string,
+    invokeid: string,
+    getRemainingTimeInMillis: () => number,
+    done: ()=>{},
+    fail: ()=>{},
+    succeed: ()=>{},
+  }
 
-        const mockContext = {
+//helper function to mock the context for each test
+function createMockContext(): Context{
+    return{
+        
             callbackWaitsForEmptyEventLoop: false,
             functionName: 'mockFunctionName',
             functionVersion: 'mockFunctionVersion',
@@ -67,7 +74,62 @@ describe("Create endpoint", () => {
             done: jest.fn(),
             fail: jest.fn(),
             succeed: jest.fn(),
-        };
+        
+    }
+}
+
+
+const bcrypt = require('bcryptjs');
+jest.mock('bcryptjs');
+
+//mock the 
+jest.mock('../../util', () => ({
+
+    MongoDB: {
+      getInstance: jest.fn().mockReturnValue({
+        connect: jest.fn(),
+        disconnect: jest.fn(),
+        
+            getCollection: jest.fn().mockReturnValue({
+              findOne: jest.fn().mockReturnValueOnce({ email: 'test@test.org', password: 'test' }).mockReturnValue(null),
+              insertOne: jest.fn(),
+            }),
+      }),
+    },
+  }));
+  
+  jest.mock('../../config', () =>({
+    registrationStart: '06/02/2024',
+    registrationEnd: '06/30/2024'
+  }));
+
+
+
+describe("Create endpoint", () => {
+    it('Duplicate User', async() =>{
+
+        const mockEvent = createMockEvent({ email: 'testEmail@gmail.com', password: 'testPassword123' },
+        '/create',
+        'POST');
+        const mockContext = createMockContext();
+
+
+        const mockCallback = jest.fn();
+
+        bcrypt.hash.mockResolvedValue('hashedPassword');
+        
+        const res = await main(mockEvent, mockContext, mockCallback);
+        expect(res.statusCode).toBe(400);
+        expect(res.body).toBe('Duplicate user!');
+
+
+    })
+    it('Create a new user', async() =>{
+
+        const mockEvent = createMockEvent({ email: 'testEmail@gmail.com', password: 'testPassword123' },
+        '/create',
+        'POST');
+        const mockContext = createMockContext();
 
         const mockCallback = jest.fn();
 
@@ -78,56 +140,17 @@ describe("Create endpoint", () => {
         expect(res.body).toBe('User created!');
 
     })
-    /*it('Duplicate User', async() =>{
-        
-        const mockEvent = {
-            body: JSON.stringify({
-                email: 'testEmail@gmail.com',
-                password: 'testPassword123',
-            }),
-            headers: {},
-            multiValueHeaders: {},
-            httpMethod: 'POST',
-            isBase64Encoded: false,
-            path: '',
-            pathParameters: null,
-            queryStringParameters: null,
-            multiValueQueryStringParameters: null,
-            stageVariables: null,
-            requestContext: {} as any,
-            resource: '',
-            rawBody: JSON.stringify({
-                email: 'testEmail@gmail.com',
-                password: 'testPassword123',
-            }),
-        };
+    it('Registration time has passed', async() =>{
+        jest.useFakeTimers();
+        jest.setSystemTime(new Date('07/01/2024'));
 
-        const mockContext = {
-            callbackWaitsForEmptyEventLoop: false,
-            functionName: 'mockFunctionName',
-            functionVersion: 'mockFunctionVersion',
-            invokedFunctionArn: 'mockInvokedFunctionArn',
-            awsRequestId: 'mockRequestId',
-            logGroupName: 'mockLogGroupName',
-            logStreamName: 'mockLogStreamName',
-            memoryLimitInMB: '128',
-            invokeid: 'mockInvokeId',
-            getRemainingTimeInMillis: () => 1000,
-            done: jest.fn(),
-            fail: jest.fn(),
-            succeed: jest.fn(),
-        };
+        const mockEvent = createMockEvent({ email: 'testEmail@gmail.com', password: 'testPassword123' }, '/create',
+        'POST');
+        const mockContext = createMockContext();
         const mockCallback = jest.fn();
-
-        bcrypt.hash.mockResolvedValue('hashedPassword');
-        MongoDB.getInstance.getCollection().mockReturnValue({
-            findOne: () => Promise.resolve({ email: 'testEmail@gmail.com' }),
-            insertOne: () => Promise.resolve("null")
-        });
         const res = await main(mockEvent, mockContext, mockCallback);
-        expect(res.statusCode).toBe(400);
-        expect(res.body).toBe('Duplicate user!');
 
-
-    })*/
+        expect(res.statusCode).toBe(403);
+        expect(res.body).toBe('Registration is closed!');
+    })
 })
