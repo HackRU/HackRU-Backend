49c532a6da9dd75aa248ec83698433d600136c75
Ethan Nguyen <en256@scarletmail.rutgers.edu>
2024-06-30 22:52:14 -0400

    fixed /update logic and removed console.log

--- src/functions/update/handler.ts
+++ src/functions/update/handler.ts
@@ -79,8 +79,8 @@ const update: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) 
     // call updates
     // directors/organizers can update anyone, hackers can only update themselves
     if (authUser.role['director'] || authUser.role['organizer'])
-      await users.updateOne({ email: event.body.user_email }, { $set: event.body.updates });
-    else if (authUser.role['hacker']) await users.updateOne({ email: authUser.email }, { $set: event.body.updates });
+      await users.updateOne({ email: event.body.user_email }, event.body.updates);
+    else if (authUser.role['hacker']) await users.updateOne({ email: authUser.email }, event.body.updates);
 
     return {
       statusCode: 200,
