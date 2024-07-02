bd173f8746941e0a3d6b57e955ce0d5c29253af0
Ethan Nguyen <en256@scarletmail.rutgers.edu>
2024-07-02 00:03:27 -0400

    accidentally deleted index.ts

---
+++ src/functions/index.ts
@@ -0,0 +1,6 @@
+export { default as hello } from './hello';
+export { default as authorize } from './authorize';
+export { default as attendEvent } from './attend-event';
+export { default as create } from './create';
+export { default as update } from './update';
+export { default as waiver } from './waiver';
