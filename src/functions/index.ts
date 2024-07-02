42b7e16dd5a95cf6f6b366b0ac4d3c4baeff6cdc
DaStormer <dastormer@stormdevelopmentz.xyz>
2024-07-02 10:23:06 -0400

    add discord to user doc in create

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -3,3 +3,4 @@ export { default as authorize } from './authorize';
 export { default as attendEvent } from './attend-event';
 export { default as create } from './create';
 export { default as update } from './update';
+export { default as discord } from './discord';
