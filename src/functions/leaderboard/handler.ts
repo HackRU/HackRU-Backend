ca26f4bb25356b69ab6dfb0b9092a8ea2955505c
poojakedia <poojakedianov@gmail.com>
2024-09-15 10:05:11 -0400

    feat: leaderboard- initial codebase

---
+++ src/functions/leaderboard/handler.ts
@@ -0,0 +1,40 @@
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import { middyfy } from '@libs/lambda';
+import { MongoDB } from '../../util';
+import * as path from 'path';
+import * as dotenv from 'dotenv';
+dotenv.config({ path: path.resolve(process.cwd(), '.env') });
+
+const leaderboard: APIGatewayProxyHandler = async () => {
+  try {
+    const db = MongoDB.getInstance(process.env.MONGO_URI);
+    await db.connect();
+    const points = db.getCollection('f24-points-syst');
+
+    const cursor = points.find().sort({ total_points: -1 }).limit(20);
+    const topPlayers = await cursor.toArray();
+
+    if (topPlayers.length === 0) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ message: 'No players found' }),
+      };
+    }
+    return {
+      statusCode: 200,
+      body: JSON.stringify(topPlayers),
+    };
+  } catch (error) {
+    console.error('Error creating user', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        statusCode: 500,
+        message: 'Internal Server Error',
+        error,
+      }),
+    };
+  }
+};
+
+export const main = middyfy(leaderboard);
