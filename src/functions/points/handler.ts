6acf1593d1e3fd7a61871d69f0c4e2d3bb8a2062
ayoobf <rflooivl@gmail.com>
2024-09-14 20:40:11 -0400

    ran prettier

--- src/functions/points/handler.ts
+++ src/functions/points/handler.ts
@@ -1,17 +1,17 @@
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
 import schema from './schema';
-import { MongoDB, validateToken } from "../../util";
+import { MongoDB, validateToken } from '../../util';
 import * as path from 'path';
 import * as dotenv from 'dotenv';
-dotenv.config({ path: path.resolve(process.cwd(), '.env') })
+dotenv.config({ path: path.resolve(process.cwd(), '.env') });
 
 // email verification regex
 const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
 const points: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
   const email = event.body.email.toLowerCase();
-  
+
   try {
     // check if email is valid. (Explicit per Ethan's /points writeup)
     if (!emailRegex.test(email)) {
@@ -23,7 +23,6 @@ const points: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) 
         }),
       };
     }
-    
 
     // check token
     const isValidToken = validateToken(event.body.auth_token, process.env.JWT_SECRET, email);
@@ -36,14 +35,12 @@ const points: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) 
         }),
       };
     }
-  
-  
+
     // Connect to DB
     const db = MongoDB.getInstance(process.env.MONGO_URI);
     await db.connect();
     const users = db.getCollection('users');
     const pointsCollection = db.getCollection('f24-points-syst');
-
 
     // Make sure user exists
     const user = await users.findOne({ email: email });
@@ -57,16 +54,14 @@ const points: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) 
       };
     }
 
-
     // TODO: Find user's points in "pointsCollection"
 
     // TODO: Validate if user's points exist
 
     // TODO: Return user's points data with stat code 200
     return {};
-  
   } catch (error) {
-    console.error('Error retrieving user points: ', error)
+    console.error('Error retrieving user points: ', error);
     return {
       statusCode: 500,
       body: JSON.stringify({
