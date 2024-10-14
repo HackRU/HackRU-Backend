5d2aa089835748e2f4e4667c6760f1b9c84080bb
DaStormer <daonlystormer@gmail.com>
2024-10-14 11:12:10 -0400

    fix(attend-event): no previous event attendance

--- src/functions/attend-event/handler.ts
+++ src/functions/attend-event/handler.ts
@@ -127,7 +127,7 @@ const attendEvent: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (ev
       body: JSON.stringify({
         statusCode: 200,
         message: 'user successfully checked into event',
-        attendance: attendEvent.day_of.event[hackEvent].attend + 1,
+        attendance: attendEvent.day_of.event?.[hackEvent]?.attend ? attendEvent.day_of.event[hackEvent].attend + 1 : 1,
       }),
     };
   } catch (error) {
