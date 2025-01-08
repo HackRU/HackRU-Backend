68a1cbc380301e2d9c082e61bb3c072b8975e86b
Ayoob <ayoobflorival@gmail.com>
2025-01-07 22:26:56 -0500

    Hardcoded email

--- src/functions/send-email/index.ts
+++ src/functions/send-email/index.ts
@@ -3,7 +3,7 @@ import { handlerPath } from '@libs/handler-resolver';
 export default {
   handler: `${handlerPath(__dirname)}/handler.main`,
   environment: {
-    SENDER_EMAIL: process.env.SENDER_EMAIL,
+
   },
   events: [
     {
