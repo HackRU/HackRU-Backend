ebdc8ba2f3b6677a8f654de8089dd9bc09ab0337
RyamL1221 <ryaml1221@gmail.com>
2025-03-06 14:52:59 -0500

    added default value for all in schema

--- src/functions/read/schema.ts
+++ src/functions/read/schema.ts
@@ -4,7 +4,7 @@ export default {
     auth_email: { type: 'string', format: 'email' },
     auth_token: { type: 'string' },
     email: { type: 'string', format: 'email' },
-    all: { type: 'boolean' }
+    all: { type: 'boolean', default: false },
   },
   required: ['auth_email', 'auth_token', 'email'],
 } as const;
